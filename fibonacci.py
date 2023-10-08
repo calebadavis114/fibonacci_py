14a6fcbf67c04868ec3197dd9125cfbfc49678a9
calebadavis114 <caleb.a.davis114@gmail.com>
2023-10-08 06:56:35 -0700

    completed

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,12 @@
 def fibonacci(n):
-  pass
-
-
+  f = 2 
+  fac = [0, 1]
+  if(n == 0):
+    return 0
+  elif(n == 1):
+    return 1
+  else:
+    for x in range(n - 2):
+      fac.append(f)
+      f = fac[-1] + fac[-1 - 1]
+  return fac[-1]
